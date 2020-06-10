71e943ef56276beaceecd51398d91536a5399acf
4l3x4ndre <alexandre@amrani.fr>
2020-06-10 23:23:04 +0200

    rows

--- lib/test1.dart
+++ lib/test1.dart
@@ -7,58 +7,73 @@ class Test1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Align(
-          alignment: Alignment.bottomCenter,
-          child: FlatButton(
-            onPressed: (() => print("a")),
-            color: Colors.red,
-            child: Text(
-              "a"
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[ // the column widget uses the children property
+            Container( // Display Cont
+              constraints: BoxConstraints.expand( // Cr
+                height: 100
+              ),
+              alignment: Alignment.bottomRight, // Aligning the text to the bottom right of our display screen
+              color: Colors.white, // Seting the background color of the container
+              child: Text(
+                "0",
+                style: TextStyle( // Styling the text
+                  fontSize: 50.0,
+                  color: Colors.black
+                ),
+                textAlign: TextAlign.right,
+              ),
             ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(18.0)
-            ),
-          ),
-          // child: GridView.count(
-          //   primary: false,
-          //   padding: const EdgeInsets.all(20),
-          //   crossAxisSpacing: 10,
-          //   mainAxisSpacing: 10,
-          //   crossAxisCount: 3,
-          //   children: List.generate(9, (index) {
-          //     return FlatButton(
-          //         onPressed: (() => print(index)),
-          //         color: Colors.red,
-          //         child: Text(
-          //           '$index'
-          //         ),
-          //         shape: RoundedRectangleBorder(
-          //           borderRadius: BorderRadius.circular(18.0),
-          //           side: BorderSide(color: Colors.red)
-          //         ),
-          //       );
-          //   }),
-            // children: <Widget>[
-            //   Container(
-            //     padding: const EdgeInsets.all(8),
-            //     child: FlatButton(
-            //       onPressed: (() => print(1)),
-            //       color: Colors.red,
-            //       child: Text(
-            //         "1"
-            //       ),
-            //       shape: RoundedRectangleBorder(
-            //         borderRadius: BorderRadius.circular(18.0),
-            //         side: BorderSide(color: Colors.red)
-            //       ),
-            //     ),
-            //   ),
-            // ],
-          // )
-        )
+            Container(
+              constraints: BoxConstraints.expand(
+                height: MediaQuery.of(context).size.height/2
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[ 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                      _button("1", () => print("C")), // using custom widget _button
+                      _button("2", () => print("0")),
+                      _button("3", () => print("=")),              
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                      _button("4", () => print("C")), // using custom widget _button
+                      _button("5", () => print("0")),
+                      _button("6", () => print("=")),           
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                      _button("7", () => print("C")), // using custom widget _button
+                      _button("8", () => print("0")),
+                      _button("9", () => print("=")),           
+                    ],
+                  ),
+                  _button("0", () => print("=")),    
+                ]
+              )
+            )
+          ],
       )
+    );
+  }
+
+  Widget _button (String number, Function() f){ // Creating a method of return type Widget with number and function f as a parameter
+    return MaterialButton(
+      height: 100.0,
+      child: Text(number,
+      style:  TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0)),
+      textColor: Colors.black,
+      color: Colors.grey[100],
+      onPressed: f,
     );
   }
 }
